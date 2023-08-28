817a55ac8f18299e519e3cfe03956d2678f8b531
cmdtjeff <cmdtjeff@gmail.com>
2023-08-28 19:56:36 -0300

    first commit

---
+++ src/main/java/com/jeff/cesaspringboot/CesaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.cesaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeff/cesaspringboot/CesaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.cesaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
